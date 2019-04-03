655325d2be24b00af9e279fa2438c665a079a579
Paula Galindo <paulagalindovf@live.com>
2019-04-03 14:42:21 -0300

    Add files via upload

---
+++ sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,22 @@
+
+public class TestaConversao {
+	public static void main(String[] args) {
+		double salario = 1270.50;
+		int valor = (int) salario;
+		System.out.println(valor);
+
+		long numeroGrande = 332323222222222l;
+		System.out.println(numeroGrande);
+		short valorPequeno = 2131;
+		System.out.println(valorPequeno);
+		byte b = 127;
+		System.out.println(b);
+
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1 + valor2;
+		System.out.println(total);
+
+	}
+
+}
